4083d9a6c0ad5a7237092c0f46e6d76e3fe41d72
OzNL <140854571+OzNL-Hub@users.noreply.github.com>
2024-06-17 02:36:37 +0300

    Create Aimbot.lua

---
+++ Aimbot.lua
@@ -0,0 +1,15 @@
+game.StarterGui:SetCore("SendNotification", {
+    Title = "OzNL Hub";
+    Text = 'Script Executed!';
+    Icon = "";
+    Duration = 4;
+})
+
+wait(1)
+
+game.StarterGui:SetCore("SendNotification", {
+    Title = "OzNL Hub";
+    Text = 'Aimbot script is under maintenance now, so please wait until the devs fix the script.';
+    Icon = "";
+    Duration = 4;
+})
