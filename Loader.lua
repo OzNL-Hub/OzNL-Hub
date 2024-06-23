f18c4e20d5c578cdfc5dc7e91268b26bc444dce9
OzNL <140854571+OzNL-Hub@users.noreply.github.com>
2024-06-23 23:33:11 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -4,7 +4,7 @@ if game.PlaceId == 7465136166 then -- Second World GPO
 
 elseif game.PlaceId == 11424731604 then -- Impel Down GPO
 
-  -- loadstring(game:HttpGet("https://raw.githubusercontent.com/OzNL-Hub/OzNL-Hub/main/Impel-Down.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/OzNL-Hub/OzNL-Hub/main/Impel-Down.lua"))()
 
 elseif game.PlaceId == 4855457388 or 5094651510 then -- Demonfall
 
