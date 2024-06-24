d6e00313959bf9a55e37030cc0987f149e958e64
OzNL <140854571+OzNL-Hub@users.noreply.github.com>
2024-06-24 22:04:06 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,14 +1,14 @@
-if game.PlaceId == 7465136166 then -- Second World GPO
+if game.PlaceId == 7465136166 then -- Second Sea GPO
 
   loadstring(game:HttpGet("https://raw.githubusercontent.com/OzNL-Hub/OzNL-Hub/main/GPO_SecondWorld.lua"))()
+
+elseif game.PlaceId == 3978370137 then -- First Sea GPO
+
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/OzNL-Hub/OzNL-Hub/main/GPO_FirstSea.lua"))()
 
 elseif game.PlaceId == 11424731604 then -- Impel Down GPO
 
   loadstring(game:HttpGet("https://raw.githubusercontent.com/OzNL-Hub/OzNL-Hub/main/Impel-Down.lua"))()
-
-elseif game.PlaceId == 3978370137 then -- Impel Down GPO
-
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/OzNL-Hub/OzNL-Hub/main/GPO_FirstSea.lua"))()
 
 elseif game.PlaceId == 4855457388 or 5094651510 then -- Demonfall
 
