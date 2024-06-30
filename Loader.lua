a49f63bdc5d9da2223c653140d00e88b1f65bd50
OzNL <140854571+OzNL-Hub@users.noreply.github.com>
2024-06-30 09:45:54 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,13 +1,12 @@
---
-if game.PlaceId == 7465136166 then -- Second Sea GPO
+if game.PlaceId == 7465136166 then -- GPO (Second Sea)
 
   loadstring(game:HttpGet("https://raw.githubusercontent.com/OzNL-Hub/OzNL-Hub/main/GPO_SecondWorld.lua"))()
 
-elseif game.PlaceId == 3978370137 then -- First Sea GPO
+elseif game.PlaceId == 3978370137 then -- GPO (First Sea)
 
   loadstring(game:HttpGet("https://raw.githubusercontent.com/OzNL-Hub/OzNL-Hub/main/GPO_FirstSea.lua"))()
 
-elseif game.PlaceId == 11424731604 then -- Impel Down GPO
+elseif game.PlaceId == 11424731604 then -- GPO (Impel Down)
 
   loadstring(game:HttpGet("https://raw.githubusercontent.com/OzNL-Hub/OzNL-Hub/main/Impel-Down.lua"))()
 
@@ -15,11 +14,19 @@ elseif game.PlaceId == 4855457388 or game.PlaceId == 5094651510 then -- Demonfal
 
   loadstring(game:HttpGet("https://raw.githubusercontent.com/OzNL-Hub/OzNL-Hub/main/Demonfall.lua"))()
 
-elseif game.PlaceId == 9224601490 then
+elseif game.PlaceId == 9224601490 then -- Fruit Battle Grounds (Main World)
 
   loadstring(game:HttpGet("https://raw.githubusercontent.com/OzNL-Hub/OzNL-Hub/main/FruitBattleGrounds.lua"))()
 
-elseif game.PlaceId == 185655149 then
+elseif game.PlaceId == 17387482786 or game.PlaceId == 13883059853 then -- Project Slayers (World2)
+
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/OzNL-Hub/OzNL-Hub/main/ProjectSlayersWorld2.lua"))()
+
+elseif game.PlaceId == 11468034852 then -- Project Slayers (Mugen Train)
+
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/OzNL-Hub/OzNL-Hub/main/ProjectSlayersMugenTrain.lua"))()
+
+elseif game.PlaceId == 185655149 then -- Blox Burg
 
   loadstring(game:HttpGet("https://raw.githubusercontent.com/OzNL-Hub/OzNL-Hub/main/Bloxburg.lua"))()
 
